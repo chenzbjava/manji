7ebdf4b83719e3e4a13ec610ac391f0259014e6d
chenzb <LeCaiQA1@LeCaitekiMac-mini-2.local>
2014-08-04 10:38:06 +0800

    Initial Commit

---
+++ imageHandbook/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  imageHandbook
+//
+//  Created by chenzb on 14-8-4.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
